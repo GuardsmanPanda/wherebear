ae6ec04f43d316e1596f94e17e7edb7440ad3768
Green Monkey Boy <greenmonkeyboy@Ankh.duck-pleco.ts.net>
2024-08-13 18:45:09 +0000

    chore: update-tailwind-colors

--- Web/Www/Shared/js/tailwind.config.js
+++ Web/Www/Shared/js/tailwind.config.js
@@ -10,61 +10,148 @@ module.exports = {
       }, colors: {
         primary: {
           surface: {
-            subtle: "#aed581", light: "#9ccc65", default: "#8bc34a", dark: "#7cb342"
-          }, border: {
-            subtle: "#7cb342", light: "#689f38", default: "#689f38", dark: "#558b2f"
-          }, text: "#33691e"
-        }, info: {
+            subtle: "#aed581",
+            light: "#9ccc65",
+            default: "#8bc34a",
+            dark: "#7cb342"
+          },
+          border: {
+            subtle: "#7cb342",
+            light: "#689f38",
+            default: "#689f38",
+            dark: "#558b2f"
+          },
+          text: "#33691e"
+        },
+        info: {
           surface: {
-            subtle: "#badbe7", light: "#97c8db", default: "#4aa4c2", dark: "#35798f"
-          }, border: {
-            subtle: "#73b6ce", light: "#4aa4c2", default: "#35798f", dark: "#205060"
-          }, text: "#35798f"
-        }, secondary: {
-          text: "#81365b", surface: {
-            subtle: "#e7c5d6", light: "#dba9c1", default: "#c47299", dark: "#b04a7b"
-          }, border: {
-            subtle: "#d090af", light: "#c47299", default: "#81365b", dark: "#56243c"
-          }
-        }, tertiary: {
-          text: "#f97316", surface: {
-            subtle: "#fff7ed", light: "#ffedd5", default: "#fed7aa", dark: "#fdba74"
-          }, border: {
-            subtle: "#fdba74", light: "#fb923c", default: "#f97316", dark: "#ea580c"
-          }
-        }, success: {
-          text: "#5fa0c9", surface: {
-            subtle: "#a0c6df", light: "#80b3d4", default: "#5fa0c9", dark: "#447695"
-          }, border: {
-            subtle: "#a0c6df", light: "#80b3d4", default: "#152936", dark: "#03090f"
-          }
-        }, error: {
+            subtle: "#badbe7",
+            light: "#97c8db",
+            default: "#4aa4c2",
+            dark: "#35798f"
+          },
+          border: {
+            subtle: "#73b6ce",
+            light: "#4aa4c2",
+            default: "#35798f",
+            dark: "#205060"
+          },
+          text: "#35798f"
+        },
+        secondary: {
+          text: "#4561bf",
           surface: {
-            subtle: "#ecb8c0", light: "#e094a2", default: "#c24a69", dark: "#8f354c"
-          }, border: {
-            subtle: "#d27085", light: "#c24a69", default: "#8f354c", dark: "#602031"
-          }, text: "#8f354c"
-        }, warning: {
-          text: "#c2410c", surface: {
-            subtle: "#fed7aa", light: "#fdba74", default: "#f97316", dark: "#ea580c"
-          }, border: {
-            subtle: "#fb923c", light: "#f97316", default: "#c2410c", dark: "#9a3412"
+            subtle: "#b2c1e9",
+            light: "#8ea3dd",
+            default: "#6d83cd",
+            dark: "#5c75c7"
+          },
+          border: {
+            subtle: "#95a5db",
+            light: "#6d83cd",
+            default: "#4561bf",
+            dark: "#3953a7"
           }
-        }, shade: {
+        },
+        tertiary: {
+          text: "#f6cca7",
           surface: {
-            subtle: "#d7ccc8", light: "#a1887f", default: "#795548", dark: "#5d4037", disabled: "#efebe9"
-          }, border: {
-            subtle: "#efebe9", light: "#d7ccc8", default: "#bcaaa4", dark: "#3e2723"
-          }, text: {
-            title: "#311e1b", subtitle: "#4e342e", body: "#6d4c41", disabled: "#a1887f", negative: "#efebe9"
+            subtle: "#fefaf6",
+            light: "#fef5ed",
+            default: "#fcebdc",
+            dark: "#fae1ca",
+            darker: "#f8d6b9"
+          },
+          border: {
+            subtle: "#fdba74",
+            light: "#fb923c",
+            default: "#f97316",
+            dark: "#ea580c"
           }
-        }, reward: {
+        },
+        success: {
+          text: "#447695",
           surface: {
-            subtle: "#fef08a", light: "#fde047", default: "#facc15", dark: "#eab308"
-          }, border: {
-            subtle: "#fef08a", light: "#fde047", default: "#facc15", dark: "#eab308"
-          }, text: "#eab308"
+            subtle: "#c0d9ea",
+            light: "#a0c6df",
+            default: "#5fa0c9",
+            dark: "#447695"
+          },
+          border: {
+            subtle: "#80b3d4",
+            light: "#5fa0c9",
+            default: "#447695",
+            dark: "#2c4e64"
+          }
+        },
+        error: {
+          surface: {
+            subtle: "#ecb8c0",
+            light: "#e094a2",
+            default: "#c24a69",
+            dark: "#8f354c"
+          },
+          border: {
+            subtle: "#d27085",
+            light: "#c24a69",
+            default: "#8f354c",
+            dark: "#602031"
+          },
+          text: "#8f354c"
+        },
+        warning: {
+          text: "#c2410c",
+          surface: {
+            subtle: "#fed7aa",
+            light: "#fdba74",
+            default: "#f97316",
+            dark: "#ea580c"
+          },
+          border: {
+            subtle: "#fb923c",
+            light: "#f97316",
+            default: "#c2410c",
+            dark: "#9a3412"
+          }
+        },
+        shade: {
+          surface: {
+            subtle: "#d7ccc8",
+            light: "#a1887f",
+            default: "#795548",
+            dark: "#5d4037",
+            disabled: "#efebe9"
+          },
+          border: {
+            subtle: "#efebe9",
+            light: "#d7ccc8",
+            default: "#bcaaa4",
+            dark: "#5d4037"
+          },
+          text: {
+            title: "#311e1b",
+            subtitle: "#4e342e",
+            body: "#6d4c41",
+            disabled: "#a1887f",
+            negative: "#fcfdfc"
+          }
+        },
+        reward: {
+          surface: {
+            subtle: "#fef08a",
+            light: "#fde047",
+            default: "#facc15",
+            dark: "#eab308"
+          },
+          border: {
+            subtle: "#fef08a",
+            light: "#fde047",
+            default: "#facc15",
+            dark: "#eab308"
+          },
+          text: "#eab308"
         }
+
       },
     },
   }, plugins: [require('@tailwindcss/forms'),],
