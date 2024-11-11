92ca24146d4f6f4235b3bc423b0f1b4ff4dbb7ec
Bjørn Dons <guardsmanpanda@gmail.com>
2024-11-11 22:17:39 +0000

    small form fix

--- Web/Www/Game/View/lobby/dialog/game-settings.blade.php
+++ Web/Www/Game/View/lobby/dialog/game-settings.blade.php
@@ -1,9 +1,7 @@
 <?php declare(strict_types=1); ?>
 @php use Domain\User\Enum\BearPermissionEnum;use GuardsmanPanda\Larabear\Infrastructure\Auth\Service\BearAuthService; @endphp
 <x-bear::dialog.update hx-patch="/game/{{$game->id}}/lobby/settings">
-  @if($game->templated_by_game_id === null)
-    <x-bear::form.number id="number_of_rounds" required min="1" max="40">{{$game->number_of_rounds}}</x-bear::form.number>
-  @endif
+  <x-bear::form.number id="number_of_rounds" required min="1" max="40"  class="@if($game->templated_by_game_id === null) hidden @endif"  >{{$game->number_of_rounds}}</x-bear::form.number>
   <x-bear::form.number id="round_duration_seconds" required min="20">{{$game->round_duration_seconds}}</x-bear::form.number>
   <x-bear::form.number id="round_result_duration_seconds" required min="10">{{$game->round_result_duration_seconds}}</x-bear::form.number>
   <fieldset class="flex gap-3 px-3 border border-gray-400 pb-3 mt-2">
